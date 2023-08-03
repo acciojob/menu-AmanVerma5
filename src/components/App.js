ee7a72b26e304001d065a1e2bcd8dc571d386ba7
AmanVerma5 <amanv4775@gmail.com>
2023-08-03 14:48:23 +0530

    submitting code-Thu Aug 03 2023 14:48:20 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,21 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState } from "react";
+import Displayfood from "./Displayfood";
 
+const App=()=>{
+    let [selectedCategory,setSelectedCategory]=useState('All');
+
+    return(
+        <div className="App">
+            <h1>Our Menu</h1>
+            <div className="categories">
+                <p onClick={()=>setSelectedCategory('All')}>All</p>
+                <p onClick={()=>setSelectedCategory('breakfast')}>Breakfast</p>
+                <p onClick={()=>setSelectedCategory('lunch')}>Lunch</p>
+                <p onClick={()=>setSelectedCategory('shakes')}>Shakes</p>
+            </div>
+            <Displayfood category={selectedCategory}/>
+        </div>
+    )
+}
+
+export default App
